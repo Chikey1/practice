801e9f066f914f544f25a57f4cc466b05e2c0fb1
Sara Qi <sara.ss.qi@gmail.com>
2018-11-19 22:31:12 -0500

    precommit before react

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   get    '/signin',   to: 'sessions#new'
   post   '/signin',   to: 'sessions#create'
   delete '/logout',   to: 'sessions#destroy'
-  resources :users,   only: :new
 end
