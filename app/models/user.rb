f18dc6bb0aabc22bffbff03ad49c3c636fae79d9
Sara Qi <sara.ss.qi@gmail.com>
2018-11-27 14:59:49 -0500

    added rubo-cop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-  VALID_PASSWORD_REGEX = /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/
+  VALID_PASSWORD_REGEX = /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/.freeze
 
   has_secure_password
   validates :password, presence: true, length: { maximum: 30 },
-    format: { with: VALID_PASSWORD_REGEX }
+                       format: { with: VALID_PASSWORD_REGEX }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    name {'test name'}
-    email {'test@test.com'}
-    password {'asdf1234'}
-    password_confirmation {'asdf1234'}
+    name { 'test name' }
+    email { 'test@test.com' }
+    password { 'asdf1234' }
+    password_confirmation { 'asdf1234' }
   end
 end
